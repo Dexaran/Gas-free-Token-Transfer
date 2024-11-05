731f0c6f6babaf17df8bc05ee40185009c62ce2e
Dexaran <dexaran820@gmail.com>
2024-11-05 10:08:11 +0000

    Update GasFreeTokenTransfer.sol

--- GasFreeTokenTransfer.sol
+++ GasFreeTokenTransfer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.16;
-pragma abicoder v2;
+pragma solidity ^0.8.2;
 
 abstract contract IERC223 {
     function name()        public view virtual returns (string memory);
@@ -16,23 +15,34 @@ abstract contract IERC223 {
 
 contract GasFreeTokenTransfer
 {
-    struct TxParams
-    {
-        uint256 fee;
-        address destination;
-        bytes   call_data;
-    }
+    uint256 public amount_received;
 
     function tokenReceived(address _from, uint256 _amount, bytes calldata _data) public returns (bytes4)
     {
         require(_data.length > 0);
+        /*
         TxParams memory transaction = abi.decode(_data, (TxParams));
 
         // Process fee payment.
         IERC223(msg.sender).transfer(block.coinbase, transaction.fee, hex"000000");
-        
+
         // Process sending to destination.
         IERC223(msg.sender).transfer(transaction.destination, _amount - transaction.fee, transaction.call_data);
+        */
+        amount_received = _amount;
+        (bool success, bytes memory _ret) = address(this).delegatecall(_data);
+        delete(_ret);
+        delete(amount_received);
+        assert(success);
         return 0x8943ec02;
+    }
+
+    function processTransferWithFee(address _destination, uint256 _fee, bytes calldata _data) public
+    {
+        // Process fee payment.
+        IERC223(msg.sender).transfer(block.coinbase, _fee, hex"000000");
+
+        // Process sending to destination.
+        IERC223(msg.sender).transfer(_destination, amount_received - _fee, _data);
     }
 }
